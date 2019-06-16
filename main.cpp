f77865be9547b28fe582d1b4fbe7d3f14a2766f8
Pat Gulotta <pgulotta@gmail.com>
2019-06-15 19:55:55 -0500

    simple examples

--- main.cpp
+++ main.cpp
@@ -1,67 +1,28 @@
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include <functional>
 #include <iostream>
+#include <future>
+#include <exception>
+#include <stdexcept>
 
+using namespace std;
 
-typedef std::function<double( double )> HyperbolicFunc;
-
-
-std::vector<HyperbolicFunc> funcs = {
-  sinh,
-  cosh,
-  tanh,
-  []( double x )
-  {
-    return x * x;
-  }
-};
-
-std::vector<HyperbolicFunc> inverseFuncs = {
-  asinh,
-  acosh,
-  atanh,
-  []( double x )
-  {
-    return exp( log( x ) / 2 );
-  }
-};
-
-
-template <typename A, typename B, typename C>
-std::function<C( A )> compose(
-  std::function<C( B )> f,
-  std::function<B( A )> g )
+int calculate()
 {
-  return [f, g]( A x ) {
-    return f( g( x ) );
-  };
+  throw runtime_error( "Exception thrown from calculate()." );
 }
 
-auto main() -> int
+int main()
 {
-  std::vector<HyperbolicFunc> composedFuncs;
-  std::vector<double> nums;
+  // Use the launch::async policy to force asynchronous execution.
+  auto myFuture = async( launch::async, calculate );
 
-  for ( int i = 1; i <= 5; ++i ) {
-    nums.push_back( i * 0.2 );
-  }
+  // Do some more work...
 
-
-  transform(
-    begin( inverseFuncs ),
-    end( inverseFuncs ),
-    begin( funcs ),
-    back_inserter( composedFuncs ),
-    compose<double, double, double> );
-
-  for ( auto num : nums ) {
-    for ( auto func : composedFuncs ) {
-      std::cout << "f(g(" << num << ")) = " << func( num ) << std::endl;
-    }
-
-    std::cout << "---------------" << std::endl;
+  // Get the result.
+  try {
+    int result = myFuture.get();
+    cout << result << endl;
+  } catch ( const exception& ex ) {
+    cout << "Caught exception: " << ex.what() << endl;
   }
 
   return 0;
